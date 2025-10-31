a149cccb062a2b89b3f2b533e493cc3a9adf445e
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-10-31 12:42:25 -0500

    Modified routes - root leads to home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
+  resources :cards, only: %i[ index show ]
+  resources :card_sets, only: %i[ index show ]
+
   get "card_attacks/index"
   get "card_attacks/show"
-  root to: "cards#index"
-  resources :cards, only: [ :index, :show ]
   get "attacks/index"
   get "attacks/show"
-
   get "images/index"
   get "images/show"
   get "supertypes/index"
   get "supertypes/show"
-  get "sets/index"
-  get "sets/show"
   get "rarities/index"
   get "rarities/show"
   get "pokemon_types/index"
   get "pokemon_types/show"
-  get "cards/show"
   get "about" => "about#about", as: "about"
   get "up" => "rails/health#show", as: :rails_health_check
 end
